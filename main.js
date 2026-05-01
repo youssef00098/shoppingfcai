32aedc916dd7d6f8731d72c903d8e9c28364fce1
emamm22 <memam4868@gmail.com>
2026-05-02 01:47:03 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-
 document.getElementById("show").onclick = function () {
     document.body.classList.toggle("dark-mode");
 };
-
-
+function changeimage(img){
+    document.getElementById("mainimage").src =img.src;
+}
+function aadtocart(){
+    alert("product added to cart!");
+}
