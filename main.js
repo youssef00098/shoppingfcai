0cda0fd66624226cdbb8f5f1f8c36141c0b67280
youssef refaat <refaatyoussef55@gmail.com>
2026-05-02 18:17:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-document.getElementById("show").onclick = function () {
-    document.body.classList.toggle("dark-mode");
-};
-function changeimage(img){
-    document.getElementById("mainimage").src =img.src;
+const darkModeBtn = document.getElementById("show");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
 }
-function aadtocart(){
+
+darkModeBtn.onclick = function () {
+    document.body.classList.toggle("dark-mode");
+    
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+};
+
+function changeimage(img) {
+    document.getElementById("mainimage").src = img.src;
+}
+
+function aadtocart() {
     alert("product added to cart!");
 }
