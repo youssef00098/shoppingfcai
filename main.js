ca2c1200474f975a964029e239f6f8991107733b
youssef refaat <refaatyoussef55@gmail.com>
2026-05-02 18:20:34 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,9 @@
-const darkModeBtn = document.getElementById("show");
-
-if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark-mode");
-}
-
-darkModeBtn.onclick = function () {
+document.getElementById("show").onclick = function () {
     document.body.classList.toggle("dark-mode");
-    
-    if (document.body.classList.contains("dark-mode")) {
-        localStorage.setItem("theme", "dark");
-    } else {
-        localStorage.setItem("theme", "light");
-    }
 };
-
-function changeimage(img) {
-    document.getElementById("mainimage").src = img.src;
+function changeimage(img){
+    document.getElementById("mainimage").src =img.src;
 }
-
-function aadtocart() {
+function aadtocart(){
     alert("product added to cart!");
 }
